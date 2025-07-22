fbb1f077569c5e69858b55188275ef27952015e9
BankThanomsup <johndoe@example.com>
2025-07-23 02:56:17 +0700

    add cookie parser

--- React-camping-workshop-Dev.Bank/server/server.js
+++ React-camping-workshop-Dev.Bank/server/server.js
@@ -7,13 +7,13 @@ const morgan = require('morgan');
 const handleErrors = require('./middlewares/error');
 require('dotenv/config')
 const { clerkMiddleware } = require ("@clerk/express")
-
-
+const cookieParser = require('cookie-parser');
 
 //middleware
 app.use(cors());  //ติดไว้ก่อน
 app.use(express.json({limit:'10mb'}))
 app.use(morgan('dev'))
+app.use(cookieParser());
 // middleware
 app.use(clerkMiddleware());
 readdirSync('./routes').map((e)=>app.use('/api',require(`./routes/${e}`)))
