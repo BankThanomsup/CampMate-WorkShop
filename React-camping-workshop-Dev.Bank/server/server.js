a0bf94cd33de01e7ed3806a0a4f1c3462e2a14d9
BankThanomsup <johndoe@example.com>
2025-07-23 03:18:42 +0700

    ีupdate server.js

--- React-camping-workshop-Dev.Bank/server/server.js
+++ React-camping-workshop-Dev.Bank/server/server.js
@@ -7,10 +7,13 @@ const morgan = require('morgan');
 const handleErrors = require('./middlewares/error');
 require('dotenv/config')
 const { clerkMiddleware } = require ("@clerk/express")
-const cookieParser = require('cookie-parser');
-
+const cookie = require("cookie");
 //middleware
-app.use(cookieParser());
+app.use((req, res, next) => {
+  const cookies = cookie.parse(req.headers.cookie || "");
+  req.cookies = cookies;
+  next();
+});
 app.use(cors({
   origin: 'https://react-camping-470y4ooot-bankthanomsups-projects.vercel.app', // กำหนดให้ชัดเจน
   credentials: true,
@@ -25,5 +28,5 @@ app.use(handleErrors)
 
 
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{console.log(`server is running on port: ${PORT}`)})
